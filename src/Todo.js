cbc3aaf159845301e3979e9580ef7ef5550b9364
ZILLABB <107042060+ZILLABB@users.noreply.github.com>
2022-11-21 03:27:35 +0800

    second commit

--- src/Todo.js
+++ src/Todo.js
@@ -1,15 +1,20 @@
 import moment from "moment/moment";
 import React, { useState } from "react";
 import { GrAddCircle } from "react-icons/gr";
+import check from "./images/Group 158.svg";
+import thumbnail from "./images/Group 159.svg";
+import checkList from "./images/Checklist-rafiki 1.svg";
+import Calendar from "react-calendar";
 
 const Todo = () => {
   const [todos, setTodos] = useState([]);
-  const [newTask, setNewTask] = useState("");
+  const [newTask, setNewTask] = useState({});
   const [input, setInput] = useState(false);
+  const [date, setDate] = useState(new Date());
 
   // let newTime = {};
-  let formattedTime = moment("".time).format("MMM DD YYYY  HH:mm");
-  console.log();
+  // let formattedTime = moment("".time).format("MMM DD YYYY  HH:mm");
+  // console.log();
 
   const handleChange = (event) => {
     setNewTask(event.target.value);
@@ -47,37 +52,49 @@ const Todo = () => {
   };
 
   return (
-    <div className="bg-gradient-to-r from-purple-500 to-blue-500">
+    <div className="bg-gradient-to-r from-purple-500 to-black rounded-t-2xl">
       <div className="text-center  justify-center m-auto  ">
-        <h1 className="text-[40px] lg:w-[200px] font-4xl text-white border-4  shadow-xl rounded-2xl p-3 ">
-          {formattedTime}
-        </h1>
-        <div className="">
-          <h1 className="text-3xl">Add a task</h1>
-          <GrAddCircle
-            className="w-10 h-10 m-auto mt-4 "
-            onClick={() => setInput(!input)}
-          />
+        <div className="flex mb-16  justify-between p-3">
+          <img src={check} alt="" />
+          <h1 className="text-white">Index</h1>
+          <img src={thumbnail} alt="" />
         </div>
 
-        {input && (
-          <form action="" className="">
-            <input
-              type="text"
-              placeholder="Add New Task "
-              className="h-10 pl-5 lg:rounded-l-xl rounded-md border-2 mt-20 lg:w-[300px] w-[300px]  "
-              onChange={handleChange}
-              required
-            />
-            <button
-              type="submit"
-              onClick={addTask}
-              className="bg-green-400 h-10 text-xl w-[150px] text-white lg:rounded-r-xl rounded-xl mt-2"
-            >
-              Add New Task
-            </button>
-          </form>
-        )}
+        <div className="">
+          <img src={checkList} className="mx-auto" alt="" />
+        </div>
+        {/* <h1 className="text-[40px] lg:w-[200px] font-4xl text-white border-4 lg:text-center justify-center  shadow-xl rounded-2xl p-3 ">
+          {formattedTime}
+        </h1> */}
+        <div className="w-[375px] text-white bg-[#363636] shadow-xl ">
+          {input && (
+            <form action="submit" className="">
+              <h1>Add Task</h1>
+              <input
+                type="text"
+                placeholder=" title"
+                className="h-[45px] lg:rounded-l-xl rounded-md border-1 opacity-4 pl-3 border-white mt-20 bg-slate-800 lg:w-[300px] w-[325px]  "
+                onChange={handleChange}
+                required
+              />
+              <input
+                type="text"
+                placeholder=" description"
+                className="h-[45px] lg:rounded-l-xl rounded-md border-1 opacity-4 pl-3 border-white mt-20 bg-slate-800 lg:w-[300px] w-[325px]  "
+                onChange={handleChange}
+                required
+              />
+              <button
+                type="submit"
+                onClick={addTask}
+                className="bg-green-400 h-10 text-xl w-[150px] text-white lg:rounded-r-xl rounded-xl mt-2"
+              >
+                Add New Task
+              </button>
+            </form>
+          )}
+        </div>
+
         <div>
           {todos &&
             todos.map((todo, index) => {
@@ -107,6 +124,18 @@ const Todo = () => {
                 </div>
               );
             })}
+          <div className="text-white">
+            <div>
+              <Calendar onChange={setDate} value={date} />
+            </div>
+            <div>{date.toDateString()}</div>
+          </div>
+        </div>
+        <div className=" w-10 rounded-full m-auto bg-gray-50">
+          <GrAddCircle
+            className="w-10  h-10 m-auto mt-4 "
+            onClick={() => setInput(!input)}
+          />
         </div>
       </div>
     </div>
